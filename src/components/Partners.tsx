550cb1ea216106a7d5ca77a61b8e8fe0bf989681
ayiteywoods <51957291+ayiteywoods@users.noreply.github.com>
2025-04-28 11:09:28 +0000

    Updated Partners

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -7,9 +7,9 @@ const Partners = () => {
         <div className='justify-center items-center'>
             <h1 className='text-3xl font-thin text-center p-4'>Our Partners</h1>
             <p className='text-center px-20 pb-5'>
-            We're proud to collaborate with industry leaders and innovators who share our commitment to excellence. 
+            We&apos;re proud to collaborate with industry leaders and innovators who share our commitment to excellence. 
       These strategic partnerships amplify our impact, drive innovation, and deliver exceptional value to our 
-      clients. Together, we're shaping the future of [your industry] through shared expertise and vision.</p>
+      clients. Together, we&apos;re shaping the future of [your industry] through shared expertise and vision.</p>
         </div>
         <div className="flex items-center justify-center gap-4">
   <div className="flex items-center justify-center">
